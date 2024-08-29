74a725f91a5867942a60174d671c4cde19789b2f
redesyef <ymauriciop@gmail.com>
2024-08-28 19:40:47 -0500

    UPDATE lead clickable

--- src/components/atoms/Button.tsx
+++ src/components/atoms/Button.tsx
@@ -4,13 +4,20 @@ type button = {
   type: 'submit' | 'reset' | 'button' | undefined;
   onClick?: () => void;
   color?: string;
+  disabled?: boolean;
 };
-const Button = ({ name, type, onClick, color = 'bg-primary' }: button) => {
+const Button = ({
+  name,
+  type,
+  onClick,
+  color = 'bg-primary',
+  disabled = false,
+}: button) => {
   return (
     <div
       className={` rounded-md  bg-opacity-90 hover:bg-opacity-100 text-white inline-block px-4 py-1  ${color}`}
     >
-      <button type={type} onClick={onClick}>
+      <button disabled={disabled} type={type} onClick={onClick}>
         <p>{name}</p>
       </button>
     </div>
