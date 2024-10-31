06fb5f9600ec65f63f315290b8658cc15190fc49
redesyef <ymauriciop@gmail.com>
2024-10-31 10:30:37 -0500

    UPDATE edit status lead in lawyer

--- src/app/(dashboard)/lawyer-management/[id]/IdLawyer.tsx
+++ src/app/(dashboard)/lawyer-management/[id]/IdLawyer.tsx
@@ -1,4 +1,8 @@
 'use client';
+import Button from '@/components/atoms/Button';
+import Input from '@/components/atoms/Input';
+import CountdownTimer from '@/components/organisms/CountdownTimer';
+import Modal from '@/components/organisms/Modal';
 import NoData from '@/components/organisms/NoData';
 import SortableTable from '@/components/organisms/SortableTable';
 import Tilte from '@/components/organisms/Tilte';
@@ -6,6 +10,7 @@ import { statusColors } from '@/configs/statusColor';
 import { database } from '@/services/database';
 import { useLeadsStore } from '@/store/useLead.store';
 import { useSelectStatus } from '@/store/useSelectStatus';
+import dayjs from 'dayjs';
 import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import { MdOutlineCases } from 'react-icons/md';
@@ -13,12 +18,49 @@ import { MdOutlineCases } from 'react-icons/md';
 const IdLawyer = ({ params }: { params: { id: string } }) => {
   const [lawyerData, setLawyerData] = useState<any>(null);
 
-  const { dataLeads } = useLeadsStore();
+  const { dataLeads, fetchLeads } = useLeadsStore();
   const [userId, setUserId] = useState<any>(null);
   const [columns, setColumns] = useState([]);
   const [originalData, setOriginalData] = useState([]);
   const [selectedStatus, setSelectedStatus] = useState<string | null>(null);
   const { selecArray, setSelecArray } = useSelectStatus();
+  const [isOpenLead, setIsOpenLead] = useState(false);
+  const [selectedLead, setSelectedLead] = useState<any>({});
+  const [selectStatus, setSelectStatus] = useState();
+  const statusSelect = [
+    {
+      name: 'Assigned',
+      value: 'ASSIGNED',
+    },
+    {
+      name: 'In progress',
+      value: 'IN PROGRESS',
+    },
+    {
+      name: 'New',
+      value: 'NEW',
+    },
+    {
+      name: 'Problematic',
+      value: 'PROBLEMATIC',
+    },
+    {
+      name: 'Send back',
+      value: 'LOST',
+    },
+    {
+      name: 'Retained',
+      value: 'CLOSED',
+    },
+    {
+      name: 'Disabled',
+      value: 'DISABLED',
+    },
+    {
+      name: 'Dead',
+      value: 'EXPIRED',
+    },
+  ];
   const getLawyer = async () => {
     const dataLawyerUser = await database.getLawyer(params.id);
     setUserId(dataLawyerUser.data.data);
@@ -117,19 +159,119 @@ const IdLawyer = ({ params }: { params: { id: string } }) => {
       <NoData
         text={`${userId?.firstName} ${userId?.lastName} hasn't been assigned any leads yet`}
       >
-        <MdOutlineCases size={70} color='#00234D' />
+        <MdOutlineCases size={70} color="#00234D" />
       </NoData>
     );
   }
+  const saveLeadContact = async (e: any) => {
+    e.preventDefault();
 
+    const dataUpdate = {
+      status:
+        e.target.checkbox.checked === true ? 'DISABLED' : e.target.status.value,
+      comments: e.target.comments.value,
+    };
+
+    const responseUpdate = await database.updateData(
+      `${process.env.NEXT_PUBLIC_URL_LEADS}/${selectedLead['lead id']}`,
+      dataUpdate
+    );
+    if (!responseUpdate.success) {
+      toast.error('Error updating Lead information');
+    }
+    toast.success('Lead information updated successfully');
+    setIsOpenLead(false);
+    fetchLeads();
+    getLawyer();
+  };
+  const handleContact = async (index: number) => {
+    setIsOpenLead(true);
+    if (lawyerData) {
+      setSelectedLead(lawyerData[index]);
+    }
+  };
   return (
-    <div className='flex flex-col gap-5'>
+    <div className="flex flex-col gap-5">
+      <Modal title="Lead info" isOpen={isOpenLead} setIsOpen={setIsOpenLead}>
+        <div className="px-8">
+          <p>
+            Selection date :{' '}
+            {dayjs
+              .utc(selectedLead['date'] as string)
+              .local()
+              .format('MM/DD/YYYY hh:mm a')}
+          </p>
+          <p className="text-red-500">
+            This lead will be marked as lost and will not be reinstated.
+          </p>
+          {selectedLead.status === 'ASSIGNED' && (
+            <CountdownTimer targetDate={selectedLead['date_updated']} />
+          )}
+          <p className="text-4xl py-6 ">{selectedLead?.['lead name']}</p>
+          <form onSubmit={saveLeadContact} className="grid grid-cols-3 gap-2 ">
+            <p className="">Status:</p>
+
+            <Input
+              type="select"
+              name="status"
+              values={statusSelect}
+              statusColors={statusColors}
+              defaultValue={selectedLead?.status}
+              setStatusSelected={setSelectStatus}
+            />
+            <p></p>
+            <p>Email:</p>
+            <p className="col-span-2 text-gray-500 ">{selectedLead?.email}</p>
+            <p>Phone number:</p>
+            <p className="col-span-2 text-gray-500">
+              {selectedLead?.['phone number']}
+            </p>
+            <p>Service Type:</p>
+            <p className="col-span-2 text-gray-500">{selectedLead?.service}</p>
+            <p>Description:</p>
+            <p className="col-span-2 text-gray-500">
+              {selectedLead?.['description lead']}
+            </p>
+            <p>Comment:</p>
+            <textarea
+              name="comments"
+              className="col-span-2 text-gray-500 border-2 bg-gray-100 rounded-md"
+              placeholder=" Leave your comment....."
+              required={selectStatus === 'LOST' ? true : false}
+            >
+              {selectedLead?.comments}
+            </textarea>
+            <p></p>
+            <p className="flex gap-1 col-span-2 text-gray-500 ">
+              <input
+                name="checkbox"
+                id={`checkbox-lead`}
+                className="peer hidden"
+                type="checkbox"
+              />
+              <label
+                htmlFor={`checkbox-lead`}
+                className="flex items-center justify-center w-5 h-5 border border-green-500 rounded bg-white cursor-pointer relative  text-white peer-checked:text-green-500"
+              >
+                <i className="fi fi-rr-check absolute  text-sm  peer-checked:block "></i>
+              </label>{' '}
+              Do not contact this lead again
+            </p>
+            <div className="col-end-4 text-right">
+              <Button name="Save" type="submit" />
+            </div>
+          </form>
+          <p className="text-red-500 text-sm py-4">
+            The super admin will review this case, leave us a clear comment.
+          </p>
+        </div>
+      </Modal>
       <Tilte
         name={`${userId?.firstName} ${userId?.lastName}`}
         search={true}
         filterSearch={filterSearch}
       />
-      <div className='flex flex-wrap  gap-2'>
+      <div className="flex flex-wrap  gap-2">
         <button
           onClick={() => handleStatusClick(null)}
           className={`px-4 p-1 rounded text-sm ${
@@ -158,6 +300,7 @@ const IdLawyer = ({ params }: { params: { id: string } }) => {
         columns={columns}
         data={lawyerData}
         statusColors={statusColors}
+        onEdit={handleContact}
       />
     </div>
   );
